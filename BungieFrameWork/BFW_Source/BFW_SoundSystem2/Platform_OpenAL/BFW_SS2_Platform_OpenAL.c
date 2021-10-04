dcbb8979377a0b49e9fd7f422cda08e991b83b0f
Alexander 'z33ky' Hirsch <1zeeky@gmail.com>
2021-10-04 03:29:21 +0200

    [!] Third stab at audio decoding for OpenAL
    Also improves some OpenAL code.
    Looping sounds may not be handled correctly yet.
    Looks like there's a memory leak when using libav.

--- BungieFrameWork/BFW_Source/BFW_SoundSystem2/Platform_OpenAL/BFW_SS2_Platform_OpenAL.c
+++ BungieFrameWork/BFW_Source/BFW_SoundSystem2/Platform_OpenAL/BFW_SS2_Platform_OpenAL.c
@@ -21,6 +21,11 @@
 #include "AL/alc.h"
 #include <math.h>
 
+#include <libavutil/frame.h>
+#include <libavutil/mem.h>
+
+#include <libavcodec/avcodec.h>
+
 // ======================================================================
 // defines
 // ======================================================================
@@ -123,7 +128,9 @@ void
 SS2rPlatform_SoundChannel_Play(
 	SStSoundChannel				*inSoundChannel)
 {
+	alSourcei(inSoundChannel->pd.source, AL_LOOPING, SSiSoundChannel_IsLooping(inSoundChannel) == UUcTrue ? AL_TRUE : AL_FALSE);
 	alSourcePlay(inSoundChannel->pd.source);
+	SSiSoundChannel_SetPlaying(inSoundChannel, UUcTrue);
 }
 
 // ----------------------------------------------------------------------
@@ -141,6 +148,7 @@ SS2rPlatform_SoundChannel_SetSoundData(
 	SStSoundData				*inSoundData)
 {
 	ALenum format;
+	unsigned channels = SSrSound_GetNumChannels(inSoundData);
 	if (inSoundChannel->flags & SScSoundChannelFlag_Mono && SScBitsPerSample == 8)
 	{
 		format = AL_FORMAT_MONO8;
@@ -153,7 +161,7 @@ SS2rPlatform_SoundChannel_SetSoundData(
 	{
 		format = AL_FORMAT_STEREO8;
 	}
-	else if (inSoundChannel->flags & SScSoundChannelFlag_Stereo && SScBitsPerSample ==  16)
+	else if (inSoundChannel->flags & SScSoundChannelFlag_Stereo && SScBitsPerSample == 16)
 	{
 		format = AL_FORMAT_STEREO16;
 	}
@@ -162,10 +170,105 @@ SS2rPlatform_SoundChannel_SetSoundData(
 		return UUcFalse;
 	}
 	
-	//TODO: decompress data?
-	alBufferData(inSoundChannel->pd.buffer, format, inSoundData->data, inSoundData->num_bytes, SScSamplesPerSecond);
+	av_log_set_level(AV_LOG_DEBUG);
+	const AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_ADPCM_MS);
+	if (!codec) {
+		return UUcFalse;
+	}
+	AVCodecContext *c = avcodec_alloc_context3(codec);
+	if (!c) {
+		return UUcFalse;
+	}
+	c->codec_id = AV_CODEC_ID_ADPCM_MS;
+	c->codec_type = AVMEDIA_TYPE_AUDIO;
+	c->channels = channels;
+	c->sample_rate = SScSamplesPerSecond;
+	c->sample_fmt = AV_SAMPLE_FMT_S16;
+	c->channel_layout = 0;
+	c->bit_rate = 128000; //(c->sample_rate * BitsPerSample) / c->channels;
+	if (avcodec_open2(c, codec, NULL) < 0) {
+		return UUcFalse;
+	}
+
+	AVCodecParameters *par = avcodec_parameters_alloc();
+	avcodec_parameters_from_context(par, c);
+	par->block_align = (channels == 2) ? 1024 : 512;
+	avcodec_parameters_to_context(c, par);
+	//avcodec_parameters_free(par);
+
+	AVPacket *pkt = av_packet_alloc();
+	if (!pkt) {
+		return UUcFalse;
+	}
+
+	//TODO: apparently only a single "frame" should be sent per packet
+	char *frame = inSoundData->data;
+	char *frames_end = frame + inSoundData->num_bytes;
+	const size_t framesz = par->block_align;
+	AVFrame *decoded_frame = av_frame_alloc();
+	if (!decoded_frame) {
+		return UUcFalse;
+	}
+	//FIXME: slight overallocation
+	void *decoded = malloc((inSoundData->num_bytes + framesz) * 4);
+	size_t total = 0;
+	while(1) {
+		int ret = avcodec_receive_frame(c, decoded_frame);
+		if (ret == -11) //EAGAIN
+		{
+			if (frame == frames_end) {
+				ret = avcodec_send_packet(c, NULL);
+				if (ret != 0) {
+					return UUcFalse;
+				}
+			} else {
+				size_t left = frames_end - frame;
+				size_t sz = (framesz <= left) ? framesz : left;
+				//FIXME: can we not allocate a new buffer for every frame??
+				void *avbuf = av_memdup(frame, sz);
+				if (!avbuf) {
+					return UUcFalse;
+				}
+				ret = av_packet_from_data(pkt, avbuf, sz);
+				if (ret != 0) {
+					av_free(avbuf);
+					return UUcFalse;
+				}
+				ret = avcodec_send_packet(c, pkt);
+				av_packet_unref(pkt);
+				if (ret != 0) {
+					return UUcFalse;
+				}
+				frame += sz;
+			}
+			continue;
+		}
+		if (ret == AVERROR_EOF) {
+			break;
+		}
+		if (ret < 0) {
+			return UUcFalse;
+		}
+		int data_size = av_samples_get_buffer_size(
+			NULL,
+			decoded_frame->channels,
+			decoded_frame->nb_samples,
+			decoded_frame->format,
+			1
+		);
+		memcpy(decoded + total, decoded_frame->extended_data[0], data_size);
+		av_frame_unref(decoded_frame);
+		total += data_size;
+	}
+	av_frame_free(decoded_frame);
 	
+	alSourcei(inSoundChannel->pd.source, AL_BUFFER, 0);
+	alBufferData(inSoundChannel->pd.buffer, format, decoded, total, SScSamplesPerSecond);
 	alSourcei(inSoundChannel->pd.source, AL_BUFFER, inSoundChannel->pd.buffer);
+	free(decoded);
+	
+	avcodec_free_context(&c);
+	av_packet_free(pkt);
 	
 	return UUcTrue;
 }
@@ -180,7 +283,6 @@ SS2rPlatform_SoundChannel_SetPan(
 	// https://github.com/kcat/openal-soft/issues/194#issuecomment-392520073
 	//TODO: https://stackoverflow.com/q/20950606
 	//alSourcef(inSoundChannel->pd.source, AL_SOURCE_DISTANCE_MODEL, AL_NONE); (context fn, not buffer)
-	//looks like Oni does mixing itself, so we only use one "source" anyways?
 	alSourcef(inSoundChannel->pd.source, AL_ROLLOFF_FACTOR, 0.0f);
 	alSourcei(inSoundChannel->pd.source, AL_SOURCE_RELATIVE, AL_TRUE);
 	switch (inPanFlags)
@@ -232,6 +334,7 @@ SS2rPlatform_SoundChannel_Stop(
 	SStSoundChannel				*inSoundChannel)
 {
 	alSourceStop(inSoundChannel->pd.source);
+	SSiSoundChannel_SetPlaying(inSoundChannel, UUcFalse);
 }
 
 // ----------------------------------------------------------------------
@@ -261,6 +364,15 @@ SS2rPlatform_Initialize(
 	
 	SSgDevice = alcOpenDevice(NULL);
 	
+	//FIXME: the number of channels will be split across mono/stereo
+	ALCint attrs[] = {
+		//ALC_FREQUENCY, SScSamplesPerSecond,
+		ALC_MONO_SOURCES, SScMaxSoundChannels * 2,
+		ALC_STEREO_SOURCES, SScMaxSoundChannels * 2,
+		0
+	};
+	SSgContext = alcCreateContext(SSgDevice, &attrs);
+	
 	ALCint numMono, numStereo;
 	alcGetIntegerv(SSgDevice, ALC_MONO_SOURCES, 1, &numMono);
 	alcGetIntegerv(SSgDevice, ALC_STEREO_SOURCES, 1, &numStereo);
@@ -268,7 +380,6 @@ SS2rPlatform_Initialize(
 	if (*outNumChannels == 0) {
 		*outNumChannels = SScMaxSoundChannels;
 	}
-	SSgContext = alcCreateContext(SSgDevice, NULL);
 	
 	return UUcError_None;
 }
